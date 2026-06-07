1119bbc33fe6624b1865efc7aa70ae0263730657
dazaissi <naylalzahra09@gmail.com>
2026-06-07 23:04:04 +0700

    Delete latihanaja directory

--- latihanaja/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php  
-   $host = "localhost";
-   $user_name = "root";
-   $password = "";
-   $database = "latihan"; 
-   $koneksi = mysqli_connect($host,$user_name,$password,$database)
-              or die("Koneksi database gagal : ". mysqli_connect_error());
-?>
--- latihanaja/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  session_start();
-  session_destroy();
-  print "Thank you ...";
-  header("location:Login.php");
-?>a
